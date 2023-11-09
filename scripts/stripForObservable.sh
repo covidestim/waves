c642129e7cd987112dfb36fa6e3a5fbd73425868
rafalopespx <lopes1313@gmail.com>
2023-11-09 15:38:48 -0500

    GitIgrnore to ignore .gif files

--- scripts/stripForObservable.sh
+++
@@ -1,2 +0,0 @@
-awk -v OFS=, -F' ' \
-  'BEGIN {print "module,fips,month"} NR>8 { print $1, $3, $6 }'
--- scripts/stripForObservableHexid.sh
+++
@@ -1,2 +0,0 @@
-awk -v OFS=, -F' ' \
-  'BEGIN {print "module,hexid,month"} NR>8 { print $1, $3, $6 }'
