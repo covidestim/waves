82f71833ce8d229cbcbb194d6c98e2f33c2e7e14
nswartwo <nswartwo@gmail.com>
2025-07-09 16:14:42 -0400

    Update hexgrid file

--- scripts/computeSpeed.R
+++ scripts/computeSpeed.R
@@ -15,13 +15,12 @@ first_peak <- as.Date("2020-11-19")
 second_peak <- as.Date("2021-09-04")
 
 ### Read in the stable hex grid
-hexgrid <- readRDS(here("data-products/hexgrid.rds")) %>% 
-  mutate(hexid = as.integer(hexid))
+hexgrid <- st_read("data-products/geo-hexes/hexgrid2025.shp") 
 
 ### Load in the new observations from the CAR model
 obs <- read.csv("~/Downloads/tsa_meta30m_run_preomicron_daily.csv", 
                 sep = "\t") %>% 
-  mutate(hexid = as.integer(hexid), 
+  mutate(hexid = as.character(hexid), 
          date = as.Date(date), 
          mean = as.numeric(mean))
 
@@ -29,15 +28,13 @@ obs <- read.csv("~/Downloads/tsa_meta30m_run_preomicron_daily.csv",
 CAR_lag_first <- obs %>% filter(date %in% seq.Date(from = (first_peak - 63), 
                                                    to = first_peak, 
                                                    length.out = 64)) %>% 
-  left_join(hexgrid, by = "hexid") %>% st_as_sf() %>% 
-  rename(geometry = x)
+  left_join(hexgrid, by = "hexid") %>% st_as_sf() 
 
 
 CAR_lag_second <- obs %>% filter(date %in% seq.Date(from = (second_peak - 63), 
                                                    to = second_peak, 
                                                    length.out = 64))  %>% 
-  left_join(hexgrid, by = "hexid") %>% st_as_sf() %>% 
-  rename(geometry = x)
+  left_join(hexgrid, by = "hexid") %>% st_as_sf() 
 
 ### Set the infection threshold for the boundary definition
 inf_threshold <- 165
@@ -69,7 +66,7 @@ for(i in -63:0){
 }
 
 saveRDS(boundList, file = here("boundaryPlots/firstNewModel/boundaryDatafirst.rds"), version = 2)
-# boundList <- readRDS("boundaryPlots/first/boundaryDatafirst.rds")
+# boundList <- readRDS("boundaryPlots/firstNewModel/boundaryDatafirst.rds")
 
 ###############################################################################
 ########### CALCULATE AND PLOT DISTANCE TO NEAREST POINT ######################
@@ -80,27 +77,39 @@ saveRDS(boundList, file = here("boundaryPlots/firstNewModel/boundaryDatafirst.rd
 growth <- vector()
 speed <- vector()
 speedHex <- vector()
+recruit <- vector()
 distanceToFrontier <- list()
 for (i in 2:length(boundList)){
   growth[i-1] <- as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"])
-  speed[i-1] <- (as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"]))/as.numeric(boundList[[i-1]]["boundary length"])
-  speedHex[i-1] <- ((nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))/(boundList[[i-1]][["boundary length"]]/1000))*64.75
+  # speed[i-1] <- (as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"]))/as.numeric(boundList[[i-1]]["boundary length"])
+  # speedHex[i-1] <- ((nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))/(boundList[[i-1]][["boundary length"]]/1000))*64.75
+  recruit[i-1] <- (nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))*64.75
   x1<- st_distance(boundList[[i-1]][["boundary"]][,"geometry"], boundList[[i]][["boundary"]][,"geometry"])
   x2 <-cbind(boundList[[i-1]][["boundary"]][,c("hexid", "date")], apply(X=x1, MARGIN=1, FUN=min, na.rm = TRUE))
   colnames(x2)[3] <- "distToFront"
   distanceToFrontier[[i-1]] <- x2
 }
 
+### Create a dataframe of boundary length and growth at time t
+summary1DF <- data.frame("Days before peak" = 62:0, 
+                         "Wave edge length (km)" = as.vector(unlist(sapply(boundList, function(x) x['boundary length', drop = FALSE])))[-1]/1000, 
+                         "Recruitment rate (km2/day)" = recruit, 
+                         "Median speed (km/day)" = sapply(distanceToFrontier, function(x) median(as.numeric(unlist(x[,'distToFront', drop = FALSE])), na.rm = TRUE))/1000,
+                         check.names = FALSE
+)
+
+write.csv(summary1DF, file = here("data-products/wave1Characteristics.csv"))
+
 ### Save for future use ### 
 saveRDS(distanceToFrontier, here("boundaryPlots/firstNewModel/firstDistanceToFrontier.rds"), version = 2)
 
 ### Read in the distance data ### 
-# distanceToFrontier <- readRDS(here("boundaryPlots/firstDistanceToFrontier.rds"))
+# distanceToFrontier <- readRDS(here("boundaryPlots/firstNewModel/firstDistanceToFrontier.rds"))
 
 for(i in 1:63){
   date <- unique(distanceToFrontier[[i]]$date)
   plotDistFront <- ggplot() + 
-    geom_sf(data=hexgrid %>% rename(geometry=x), mapping=aes(geometry= geometry), fill="grey80") +
+    geom_sf(data=hexgrid, mapping=aes(geometry= geometry), fill="grey80") +
     geom_sf(data=boundList[[i]][["wave"]], mapping=aes(geometry= geometry), fill="grey50") +
     geom_sf(data=distanceToFrontier[[i]], mapping=aes(geometry= geometry, 
                                                       color=distToFront/1000), size=1.5) +
@@ -147,24 +156,42 @@ for(i in -63:0){
   dev.off()
 }
 
-saveRDS(boundList, file = "boundaryPlots/secondNewModel/boundaryDatasecond.rds", version = 2)
+saveRDS(boundList, file = "boundaryPlots/secondNewModel/boundaryData_secondWave.rds", version = 2)
 
-# boundList <- readRDS("boundaryPlots/second/boundaryDatasecond.rds")
+# boundList <- readRDS(here("boundaryPlots/secondNewModel/boundaryData_secondWave.rds"))
 
 ## calculate growth of wave front 
 growth <- vector()
 speed <- vector()
 speedHex <- vector()
+recruit <- vector()
 distanceToFrontier <- list()
 for (i in 2:length(boundList)){
   growth[i-1] <- as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"])
-  speed[i-1] <- (as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"]))/as.numeric(boundList[[i-1]]["boundary length"])
-  speedHex[i-1] <- ((nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))/(boundList[[i-1]][["boundary length"]]/1000))*64.75
-  x1<- st_distance(boundList[[i-1]][["boundary"]][,"geometry"], boundList[[i]][["boundary"]][,"geometry"])
+  # speed[i-1] <- (as.numeric(boundList[[i]]["boundary length"]) - as.numeric(boundList[[i-1]]["boundary length"]))/as.numeric(boundList[[i-1]]["boundary length"])
+  # speedHex[i-1] <- ((nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))/(boundList[[i-1]][["boundary length"]]/1000))*64.75
+  recruit[i-1] <- (nrow(boundList[[i]][["wave"]]) - nrow(boundList[[i-1]][["wave"]]))*64.75
+  x1 <- st_distance(boundList[[i-1]][["boundary"]][,"geometry"], boundList[[i]][["boundary"]][,"geometry"])
   x2 <-cbind(boundList[[i-1]][["boundary"]][,c("hexid", "date")], apply(X=x1, MARGIN=1, FUN=min, na.rm = TRUE))
   colnames(x2)[3] <- "distToFront"
   distanceToFrontier[[i-1]] <- x2
 }
+
+### Create a dataframe of boundary length and growth at time t
+summary2DF <- data.frame("Days before peak" = 62:0, 
+                      "Wave edge length (km)" = as.vector(unlist(sapply(boundList, function(x) x['boundary length', drop = FALSE])))[-1]/1000, 
+                      "Recruitment rate (km2/day)" = recruit, 
+                      "Median speed (km/day)" = sapply(distanceToFrontier, function(x) median(as.numeric(unlist(x[,'distToFront', drop = FALSE])), na.rm = TRUE))/1000,
+                      check.names = FALSE
+)
+
+write.csv(summary2DF, file = here("data-products/wave2Characteristics.csv"))
+
+### Save for future use ### 
+saveRDS(distanceToFrontier, here("boundaryPlots/secondNewModel/secondDistanceToFrontier.rds"), version = 2)
+
+
+
 ###############################################################################
 ########### CALCULATE AND PLOT DISTANCE TO NEAREST POINT ######################
 ########### ON THE FRONTIER FOR EACH LINESTRING AT EACH #######################
@@ -181,7 +208,7 @@ for(i in 1:63){
   date <- unique(distanceToFrontier[[i]]$date)
   plotDistFront <-
     ggplot() + 
-    geom_sf(data=hexgrid %>% rename(geometry=x), mapping=aes(geometry= geometry), fill="grey80") +
+    geom_sf(data=hexgrid, mapping=aes(geometry= geometry), fill="grey80") +
     geom_sf(data=boundList[[i]][["wave"]], mapping=aes(geometry= geometry), fill="grey50") +
     geom_sf(data=distanceToFrontier[[i]], mapping=aes(geometry= geometry, 
                                                       color=distToFront/1000), size=1.5) +
