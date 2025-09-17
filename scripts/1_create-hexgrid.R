608f4e0e279eacbdb8c201ab474b8b7ece0c368d
nswartwo <nswartwo@gmail.com>
2025-09-17 11:46:31 -0400

    Create new hexgrid with hexes of equal area

--- scripts/1_create-hexgrid.R
+++ scripts/1_create-hexgrid.R
@@ -1,39 +1,12 @@
+# Waves project: US hex-grid generator
+
 suppressPackageStartupMessages( library(sf) )
 suppressPackageStartupMessages( library(geojsonio) )
 library(magrittr, warn.conflicts = F)
 library(ggplot2,  warn.conflicts = F)
 library(dplyr,    warn.conflicts = F)
 library(stringr,  warn.conflicts = F)
-library(docopt,   warn.conflicts = F)
-library(cli,      warn.conflicts = F)
 library(readr,    warn.conflicts = F)
-
-'Waves project: US hex-grid generator
-
-Usage:
-  hexbin.R --save-mapping <path> [--save-shp <path>] [--save-geojson <path>] --save-neighbors <path> --county-polygons <path> --cbg-polygons <path> --cbg-popsize <path> [--hexsize <num>] [--lower-48]
-  hexbin.R (-h | --help)
-  hexbin.R --version
-
-Options:
-  --save-mapping <path>     Where to save CSV of [hexid,fips,proportion1,proportion2]
-  --save-neighbors <path>   Where to save CSV of [i,j] neighbors
-  --save-shp <path>         Where to save SHP of hexes, with fields [hexid]
-  --save-geojson <path>     Where to save GeoJSON of hexes, with fields [hexid]
-  --county-polygons <path>  Path to TopoJSON of EPSG4326 county boundaries
-  --cbg-polygons <path>     Path to TIGER SHP of CBG polygons
-  --cbg-popsize <path>      Path to CSV of CBG popsize estimates, [GEOID,population]
-  --hexsize <num>           Width of each hex, unit: mi, [default: 20]
-  --lower-48                Restrict binning to lower 48 states
-  -h --help                 Show this screen.
-  --version                 Show version.
-
-' -> doc
-
-ps <- cli_process_start
-pd <- cli_process_done
-
-args <- docopt(doc, version = 'hexbin.R 0.1')
 
 # The TopoJSON file is read in, the counties layer is selected, then the data
 # is converted to a `sf`. Some of these geometries are invalid because of how
@@ -44,16 +17,17 @@ args <- docopt(doc, version = 'hexbin.R 0.1')
 # Anyway, `st_make_valid` fixes the problems but it's apparently important to
 # reassign the result of this call in order to avoid a stale reference to the
 # unrepaired data (or at least I think that's what's going on.
-ps("Loading county polygons from {.file {args$county_polygons}}, then reprojecting")
-counties_raw <- topojson_read(args$county_polygons, layer = "counties") %>%
-  st_make_valid()
+print("Loading county polygons then reprojecting")
+counties_raw <- topojson_read("Data/data-sources/county_polygons.topojson", 
+                              layer = "counties") %>%
+                st_make_valid()
 
 # missing geometries cause hexgrid() to fail below; check for NA and print
 # the number of counties removed.
 counties_na <- counties_raw %>% tidyr::drop_na()  
 if (dim(counties_raw)[1] > dim(counties_na)[1]) {
   diff_dim <- dim(counties_raw)[1] - dim(counties_na)[1]
-  cli_progress_step(paste(diff_dim, " county(ies) dropped due to missing geometry."))
+  print(paste(diff_dim, " county(ies) dropped due to missing geometry."))
 }
 
 st_crs(counties_na) <- 4326 # This TopoJSON doesn't encode the CRS maybe
@@ -62,69 +36,58 @@ counties <- st_make_valid(counties_na)
 # this is necessary or st_make_grid fails below
 counties <- counties[!st_is_empty(counties),]
 rm(counties_raw); rm(counties_na)
-pd()
 
-if (identical(args$lower_48, T)) {
-  cli_alert_warning("Removing all county polygons which fall outside the lower-48 states")
+print("Removing all county polygons which fall outside the lower-48 states")
 
-  # Source: https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code
-  excludes = c(
+# Source: https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code
+excludes = c(
     "02", "60", "03", "81", "07", "64",
     "14", "66", "84", "86", "67", "89",
     "68", "71", "76", "69", "70", "95",
-    "43", "72", "74", "79", "15"
+    "43", "72", "74", "78", "79", "15"
   )
 
-  counties <- filter(counties, !str_sub(id, 1, 2) %in% excludes)
+counties <- filter(counties, !str_sub(id, 1, 2) %in% excludes)
+
+### Set the hexsize in kilometers 
+hexsize <- 1100
+print(paste("Hexsize will be", hexsize, "km^2"))
+print("Creating hexgrid")
+
+### transform the counties to a projection compatible with meter hexgrid 
+### create a hexgrid using kilometers 
+countiesM <- st_transform(counties, 6933)
+
+hexgrid = st_make_grid(countiesM,
+                       square = F,
+                        units::as_units(hexsize, "km^2"))  %>%
+          st_as_sf %>%
+          # Eliminate the hexes that don't intersect any counties.
+          # I.e. the hexes in the Pacific Ocean etc (b/c Hawaii)
+          st_filter(countiesM, .predicate = st_intersects) %>%
+          # Assign serial ID to each hex.
+          mutate(hexid = as.character(1:n()))
+
+###############################################################################
+### Test that the hexgrid meets our expectations  
+###############################################################################
+### All hexes are the same size to the fourth decimal
+if(length(unique(round(as.numeric(units::set_units(st_area(hexgrid),km^2)), 4))) > 1){
+  print("Hexes are not equal sizes")
+} else {
+  print(paste("All hexes are", unique(round(as.numeric(units::set_units(st_area(hexgrid),km^2)), 4)),"km^2"))
 }
 
-ps("Loading CBG polygons from {.file {args$cbg_polygons}}")
-cbgs <- read_sf(args$cbg_polygons)
-pd()
+range(as.numeric(units::set_units(st_area(hexgrid),km^2)))
 
-ps("Loading CBG popsizes from {.file {args$cbg_popsize}}")
-cbg_popsize <- read_csv(
-  args$cbg_popsize,
-  col_types = cols(GEOID = col_character(), population = col_number())
-)
-pd()
+### Total area approximates that of the United States
+### Per wikipedia, per online ~8 million kilometer squared
+paste("Total area covered by the hexgrid is", 
+round(as.numeric(units::set_units(st_area(hexgrid %>% st_union()),km^2))/1e6, 3), 
+"million km^2")
 
-hexsize <- as.numeric(args$hexsize)
-cli_alert_info("Hexsize will be ~{.val {hexsize}}mi")
-
-# Crude formula to try and make the grid size change-able in a meaningful
-# way.
-miles_to_degrees <- function(miles) {
-  earth_diameter <- 3960
-  radians_to_degrees <- 180/pi
-  (miles / earth_diameter) * radians_to_degrees
-}
-
-ps("Creating hexgrid")
-hexgrid <- st_make_grid(
-  counties,
-  square = F, # Means "create hexagons"
-  cellsize = miles_to_degrees(hexsize) %>% rep(2) # width/height
-) %>%
-  st_as_sf %>%
-  # Eliminate the hexes that don't intersect any counties.
-  # I.e. the hexes in the Pacific Ocean etc (b/c Hawaii)
-  st_filter(counties, .predicate = st_intersects) %>%
-  # Assign serial ID to each hex.
-  mutate(hexid = as.character(1:n()))
-pd()
+# ggplot() + geom_sf(data=hexgrid)
 
 # Since we'll need the hexes for graphing later on, save them to a shapefile
-if (!is.null(args$save_shp)) {
-  ps("Writing hexgrid shapefile to {.file {args$save_shp}}")
-  st_write(hexgrid, args$save_shp, append = F) # Forces overwrite
-  pd()
-}
-
-# Can also save GeoJSON if user specifies. Better for Observable/d3
-if (!is.null(args$save_geojson)) {
-  ps("Writing hexgrid GeoJSON to {.file {args$save_geojson}}")
-  geojson_write(hexgrid, geometry = "polygon", file = args$save_geojson)
-  pd()
-}
-
+print("Writing hexgrid shapefile")
+st_write(hexgrid, paste0("Data/data-products/geo-hexes/hexgrid_", hexsize, "_km.shp"), append = F) # Forces overwrite
